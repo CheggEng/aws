b4b4a9e2f9ee32271a1795433d887a5bde94d930
Aaron Collins <analogrithems@gmail.com>
2016-10-24 14:33:23 -1000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'LWRPs for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.8.1'
+version '2.8.2'
 recipe 'aws', 'Installs the aws-sdk gem during compile time'
 recipe 'ec2_hints', 'Adds an EC2 hint file for Ohai cloud detection'
 source_url 'https://github.com/chef-cookbooks/aws' if respond_to?(:source_url)
